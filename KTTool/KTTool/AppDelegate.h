fd7e5f0a693b0f09db56fdb5b4cd44df4d4ab609
Ki <372160694@qq.com>
2018-02-08 10:54:55 +0800

    init

---
+++ KTTool/KTTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KTTool
+//
+//  Created by Ki on 2018/2/8.
+//  Copyright © 2018年 Ki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
